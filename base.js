1cbbf8762d996582d6f478aa66f250bad6ac546d
daniel <57467819+DanielCW7@users.noreply.github.com>
2020-09-01 01:46:38 -0500

    cleaned up js

--- base.js
+++ base.js
@@ -1,26 +1,17 @@
 let i1 = document.getElementById('icon1');
-let i2 = document.getElementById('icon2');
 let i3 = document.getElementById('icon3');
-
-let nav_project = document.getElementById('project-nav');
-let nav_cover = document.getElementById('cover-letter-nav');
-
 let project = document.getElementById('project-container');
 let cover = document.getElementById('cover-letter');
 
 // icon events
 i1.addEventListener ('click', function() {
-    i1.style.color = "rgb(149, 47, 245)";
-    i3.style.color = "black";
-    project.style.display = 'block';
+    i1.style.color = 'rgb(149, 47, 245)';
+    project.style.display = '';
     cover.style.display = 'none';
 });
 
 i3.addEventListener ('click', function() {
-    i1.style.color = "black";
-    i3.style.color = "rgb(149, 47, 245)";
-    cover.style.display = 'block';
+    i3.style.color = 'rgb(149, 47, 245)';
+    cover.style.display = '';
     project.style.display = 'none';
 });
-
-
